5a42214385c7d6caa027808ce633c0037af348fa
Kolyan1414 <balakinkolya@gmail.com>
2017-03-05 18:38:08 +0300

    ithere was error in main

---
+++ bang1.s
@@ -0,0 +1,450 @@
+﻿.EQU SREG,0x3F			;STATUS REG.
+.EQU SPH,0x3E			;ST.POINTER(HIGH)
+.EQU SPL,0x3D			;ST.POINTER(LOW)
+	
+.EQU TIMSK,0x39			;TIME COUNT OUTPUT COMPARE MATH INTERRUPT ENAB
+.EQU TCCR0,0x33			;TIME COUNTER CONTROL REG.
+.EQU TCNT0,0x32			;T/C FLAG REG			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+.EQU OCR0,0x3C			;TOP
+.EQU GICR,0x3B			;register for int0/1 interruption
+.EQU MCUCR, 0x35		;MCU control register
+
+.EQU PORTA,0x1B			;PORTA is 1st player points
+.EQU DDRA,0x1A
+.EQU PINA,0x19
+
+.EQU PORTB,0x18			;PORTB is 2nd player points
+.EQU DDRB,0x17
+.EQU PINB,0x16
+
+.EQU PORTC,0x15			;PORTC is buttons and indicating LEDs
+.EQU DDRC,0x14
+.EQU PINC,0x13
+
+.EQU PORTD,0x12			;PORTD is punishment + interruptions
+.EQU DDRD,0x11
+.EQU PIND,0x10
+
+rjmp BOOT			;0
+rjmp INT0			;1 rjmp INT0
+rjmp INT1			;2 rjmp INT1
+nop					;3
+nop					;4
+nop					;5
+nop					;6
+nop					;7
+nop					;8
+nop;rjmp TIM0_OVR		;9 rjmp TIM0_OVR
+nop					;10
+nop					;11
+nop					;12
+nop					;13
+nop					;14
+nop					;15
+nop					;16
+nop					;17
+nop					;18
+nop					;19
+nop					;20
+
+MAIN:
+	rcall BOOT_LED
+
+	push r16
+	ser r16
+	out PORTC, r16
+	pop r16
+metk:
+	nop			;random?
+	rjmp metk
+
+BOOT:				;set MCU configuration like ports, pins, registers...
+	ldi r16, 0x2 		;setting stack pointer
+	out SPH, r16
+	
+	ldi r16, 0x5F
+	out SPL, r16
+
+	ser r16			;11111111
+	mov r0, r16
+	mov r1, r16
+	mov r2, r16
+
+	out DDRA, r16		;ALL PORTA IS OUTPUT
+	out DDRB, r16		;ALL PORTB IS OUTPUT
+	out DDRC, r16		;ALL PORTC IS OUTPUT
+	
+	clr r16
+	out PORTA, r16		;all PORTA pins are set to 0 output
+	out PORTB, r16		;all PORTB pins are set to 0 output
+	out PORTC, r16		;all PORTC pins are set to 0 output
+	
+	ldi r16, 0xF9		;r16=11110011
+	out DDRD, r16		;int0/1 are input, other are output
+	ldi r16, 0x06		;r16=00001100
+	out PORTD, r16		;input pins turn pull resistance on
+
+	;timer settings
+	;ldi r16, 0x8		;TOP is 8 <==> 1 ms 
+	;out OCR0,r16
+
+	;ldi r16, 0xD		;00001101 == cs/1024
+	;out TCCR0,r16		;interrupt in 1ms
+
+	;ldi r16, 0x1		;00000001
+	;out TIMSK,r16		;allow interrupt t0
+	
+	ldi r16,0xC0
+	out GICR, r16		;allow interrupt int1/0
+
+	ldi r16, 0x0A		;high-to-low
+	out MCUCR, r16
+
+	sei				;allow interrupt global
+
+	rjmp MAIN
+
+;Задержка = r16*r17*10^(-1)с (частота МК 8МГц)
+DELAY:		
+	push r16							
+	push r17 		
+	push r18
+	push r19
+	ldi r18, 0xFF
+	ldi r19, 0xFF
+
+	delay_iter:
+		dec r19
+		brne delay_iter					;256*
+			dec r18	
+			brne delay_iter				;256*
+				dec r17	
+				brne delay_iter			;r17*
+					dec r16
+					brne delay_iter		;r16*
+	pop r19
+	pop r18
+	pop r17
+	pop r16
+ret 									;=1/(8*10^(6)) * 256*256*1 =  0.008192
+
+;players LEDs flash in the beginning of the game
+BOOT_LED:
+	
+	push r18
+	push r19
+	push r17
+	push r16
+
+	ldi r19, 0x80
+
+LED1:
+	ldi r18, 0x01
+	and r18, r1
+	out PORTA, r18
+	ldi r18, 0x01
+	and r18, r2
+	out PORTB, r18
+CYCLE:
+	ldi r16, 0x01			;delay parameters
+	ldi r17, 0x03
+	rcall DELAY
+
+	cp r18, r19			;if the last LED is flashing
+	breq LED1			;flash the first LED
+	lsl	r18			;flash the next one
+
+	and r18, r1
+	out PORTA, r18
+	and r18, r2
+	out PORTB, r18
+
+	mov r16, r1
+	mov r17, r2
+	or r16, r17
+	cpi r16, 0
+	breq EXIT
+
+	rjmp CYCLE
+EXIT:
+
+	pop r16
+	pop r17
+	pop r19
+	pop r18
+
+	reti
+
+;ФУНКЦИЯ "led_blink"
+
+;r20 = cостояние диодов в PORTA
+;r21 = cостояние диодов в PORTB
+;r22 = cостояние диодов в PORTC
+;r23 = cостояние диодов в PORTD
+;r19 = количство миганий
+;r16 = длительность исходного состояния [10^(-1)с
+;r17 = длительность состояния противоположного исходному [10^(-1)с
+
+led_blink: 								
+	led_blink_iter:
+		rcall DELAY
+			
+		com r20
+		com r21
+		com r22
+		com r23
+		out PORTA, r20
+		out PORTB, r21
+		out PORTC, r22
+		out PORTD, r23
+		push r16
+		mov r16, r17
+		rcall DELAY
+
+		com r20
+		com r21
+		com r22
+		com r23
+		out PORTA, r20
+		out PORTB, r21
+		out PORTC, r22
+		out PORTD, r23
+		pop r16
+		
+		dec r19
+		brne led_blink_iter
+ret
+
+;ФУНКЦИЯ "random"(на основе timer0)
+;!!!Добавить в boot (если этого еще нет)
+.EQU TIMSK,0x39		;TIME COUNT OUTPUT COMPARE MATH INTERRUPT ENAB
+.EQU TCCR0,0x33		;TIME COUNTER CONTROL REG.
+.EQU TCNT0,0x32		;T/C FLAG REG
+
+	push r16
+	ldi r16,0x01								;Запуск таймера (на вход поступает тактовая частота)
+	out TCCR0,r16	
+
+	ldi r16,0x00								;Запрет обработки прерываний таймера
+	out TIMSK,r16		
+	pop r16
+;!!!
+
+;!!!PORTC все выходы
+
+RANDOM:										;Подает сигнал к началу раунда с задержкой 3+random с
+	push r16
+	push r17								;Задержка 3с после нажатия двух кнопок о готовности(перед началом рандомного времени)
+	ldi r16,0x1E
+	ldi r17,0x0C
+	rcall DELAY
+	
+	in r16,TCNT0							;Считываем время из таймера	
+	lsr r16									;Делим на 8, чтобы получить время рандомной задержки ~r16*delay
+	lsr r16
+	lsr r16
+	ldi r17,0x0B	
+	rcall DELAY							;Рандомная задержка ~r16*delay
+	
+	ldi r16,0xFF							;Зажигание сигнальных диодов(весь PORTC)
+	out PORTC,r16							
+	ldi r16,0x14							;Сигнальные диоды горят 2с
+	rcall DELAY 
+	ldi r16,0x00
+	out PORTC,r16							;Сигнальные диоды гаснут
+
+	pop r17
+	pop r16
+ret
+
+;TIM0_OVR:
+;	reti
+
+INT0:
+	push r18		;save this registers
+
+	mov r18, r0
+	cpi r18, 0x0		;if r0 == 0 ==> game is started	
+	breq THE_GAME0
+	
+	clr r1			;r1=0 ==> 1st player is ready
+	
+	clr r30
+	out PORTA, r30
+
+	mov r18, r2	
+	cpi r18, 0x0
+	brne EXIT_INT0
+	
+	clr r0
+
+	rjmp EXIT_INT0
+
+THE_GAME0:
+
+	in r18, PINC
+	
+	cpi r18, 0xFF		;if there is signal --> 1st won
+	
+	brne SECOND_WON		;otherwise 2nd won
+	
+	lsl r30  		;00001111 --> 00011111
+	inc r30
+	
+	out PORTA, r30
+
+	;rcall PUNISH_2
+
+	mov r18, r30
+	cpi r18, 0xFF
+	brne EXIT_INT0		;if <8 ==> exit; else call WIN
+	
+	rcall WIN
+
+SECOND_WON:
+	lsl r31
+	inc r31
+	out PORTB, r31
+	
+	;rcall PUNISH_1	
+
+	mov r18, r31
+	cpi r18, 0xFF
+	brne EXIT_INT0		;if <8 ==> exit; else call WIN
+	
+	rcall WIN
+
+EXIT_INT0:
+	pop r18
+
+	reti
+
+INT1:
+	push r18		;save this registers
+
+	mov r18, r0
+	cpi r18, 0x0		;if r0 == 0 ==> game is started	
+	breq THE_GAME1
+	
+	clr r2			;r2=0 ==> 2nd player is ready
+	
+	clr r31
+	out PORTB, r31
+
+	mov r18, r1	
+	cpi r18, 0x0
+	brne EXIT_INT1
+	
+	clr r0
+	
+	rjmp EXIT_INT1
+
+THE_GAME1:
+	in r18, PINC
+	
+	cpi r18, 0xFF		;if there is signal --> 2nd won
+	
+	brne FIRST_WON		;else --> 1st won
+	
+	lsl r31 		;00001111 --> 00011111
+	inc r31
+	
+	out PORTB, r31
+
+	;rcall PUNISH_1
+
+	mov r18, r31
+	cpi r18, 0xFF
+	brne EXIT_INT1		;if <8 ==> exit; else call WIN
+	
+	rcall WIN
+
+FIRST_WON:
+	lsl r30
+	inc r30
+	
+	out PORTA, r30
+	
+	;rcall PUNISH_2
+
+	mov r18, r30
+	cpi r18, 0xFF
+	brne EXIT_INT1
+	
+	rcall WIN
+
+EXIT_INT1:
+	pop r18
+
+	reti
+
+WIN:
+	cpi r16,0xFF
+	breq FIRST_PROF
+	
+	ser r16
+	out PORTB, r16
+	clr r16
+	out PORTA, r16
+
+FIRST_PROF:
+	clr r16
+	out PORTB, r16
+	ser r16
+	out PORTA, r16
+
+	ldi r17,0x0B
+	ldi r16,0x1E
+	rcall DELAY
+
+	rjmp BOOT
+
+PUNISH_1:
+	push r18							
+	push r17
+	push r16
+	in r18,PORTD						;Напряжение на PD0, земля на PD1				
+	ori r18, 0x01
+	out PORTD,r18
+	
+	ldi r17,0x03						;Мотор работает 0.1с
+	ldi r16,0x01
+	rcall DELAY
+
+	andi r18,0xFC						;Напряжение на PD1, земля на PD0
+	ori r18, 0x02
+	out PORTD,r18					
+	rcall DELAY							;Мотор работает 0,1с
+
+	andi r18,0xFC
+	out PORTD,r18						;PD0 и PD1 - земля
+
+	pop r16
+	pop r17
+	pop r18
+ret
+
+PUNISH_2:
+	push r18							
+	push r17
+	push r16
+	in r18,PORTD						;Напряжение на PD1, земля на PD2				
+	ori r18, 0x02
+	out PORTD,r18
+	
+	ldi r17,0x03						;Мотор работает 0.1с
+	ldi r16,0x01
+	rcall DELAY
+
+	andi r18,0xFC						;Напряжение на PD0, земля на PD1
+	ori r18, 0x01
+	out PORTD,r18					
+	rcall DELAY							;Мотор работает 0,1с
+
+	andi r18,0xFC
+	out PORTD,r18						;PD0 и PD1 - земля
+
+	pop r16
+	pop r17
+	pop r18
+ret
